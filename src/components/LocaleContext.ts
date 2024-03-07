870c2604f8558036cc1eb12ee46d9d4d77c53f58
deadend4 <vistanciya.toxin@mail.ru>
2024-03-07 15:22:19 +0300

    add default language on load

--- src/components/LocaleContext.ts
+++ src/components/LocaleContext.ts
@@ -1,12 +1,13 @@
 import { createContext, useContext, useReducer } from "react";
 import { Locale, Translation } from "../types";
 import ruTranslation from "../localization/ru.json";
+import { getDefaultLocale } from "../utils";
 
+const defaultLocale = getDefaultLocale();
 interface LocaleContextProps {
   locale: Locale;
   currentTranslation: Translation;
 }
-
 export const LocaleContext = createContext<LocaleContextProps>({
   locale: "ru",
   currentTranslation: ruTranslation,
@@ -15,7 +16,7 @@ export const LocaleContext = createContext<LocaleContextProps>({
 export const LocaleDispatchContext = createContext<{
   dispatch: React.Dispatch<Locale>;
 }>({
-  dispatch: (locale) => "ru",
+  dispatch: (locale) => locale,
 });
 
 export function useLocale() {
@@ -25,9 +26,9 @@ export function useLocaleDispatch() {
   return useContext(LocaleDispatchContext);
 }
 export function useLocaleReducer() {
-  return useReducer(localeReducer, "ru");
+  return useReducer(localeReducer, defaultLocale);
 }
 
-function localeReducer(locale: Locale, action: Locale) {
+export function localeReducer(_locale: Locale, action: Locale) {
   return action;
 }
--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ import ruTranslation from "./localization/ru.json";
 import enTranslation from "./localization/en.json";
 import { enUS } from "date-fns/locale/en-US";
 import { ru } from "date-fns/locale/ru";
+import availableLocales from "./localization/availableLocales";
 
 export function capitalizeFirstLetter(text: string): string {
   const newText = text[0].toUpperCase() + text.substring(1);
@@ -27,4 +28,21 @@ export function getDateLocale(locale: Locale) {
     default:
       break;
   }
+}
+
+export function getDefaultLocale() {
+  let defaultLocale: Locale = "en";
+
+  if (localStorage.getItem("locale")) {
+    defaultLocale = localStorage.getItem("locale") as Locale;
+  } else {
+    defaultLocale = getValidLocale(
+      navigator.language.substring(0, 2) as Locale
+    );
+  }
+  return defaultLocale;
+}
+
+export function getValidLocale(locale: Locale) {
+  return availableLocales.includes(locale) ? locale : "en";
 }
